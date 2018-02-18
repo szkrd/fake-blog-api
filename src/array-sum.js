087183050ea885cdeaf89f9bc470031f81bb9896
Szabolcs Kurdi <szabolcs.kurdi@gmail.com>
2018-02-18 14:55:19 +0100

    fix: console log removed

--- src/array-sum.js
+++ src/array-sum.js
@@ -1,6 +1,5 @@
 function arraySum (results, filter = '', prev = {}) {
   let filters = filter.split(',');
-  console.log(filters);
   results = results.map(item => {
     filters.forEach(filter => {
       if (Array.isArray(item[filter])) {
